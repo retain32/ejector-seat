6f247870361e3b3c8f9c5fe377694a4bbbf3b576
Seth Phillips <sphillips345@g.rwu.edu>
2018-11-16 01:43:12 -0500

    Near functioning

--- eject.py
+++ eject.py
@@ -14,11 +14,27 @@ Saved regexs
 
 
 def main():
-    pattern = re.compile("(\/dev\/disk[0-999]) \((disk image|external, physical)\)")
-    #diskutil_output = open("mock_diskutil_output.txt", "r").read()
+    diskutil_pattern = re.compile("(\/dev\/disk[0-999]) \((disk image|external, physical)\)")
     diskutil_output = check_output(["diskutil", "list"]).decode('ascii')
+    df_output = check_output(["df"]).decode('ascii')
 
-    # TODO: Add HR name to the disk
+    diskutil_list = []
+    # pattern = ""
+    for m in diskutil_pattern.finditer(diskutil_output):
+        # print("%s: %s" % (m.group(1), m.group(2)))
+        diskutil_list.append(m.group(1))
+
+    disks = {}
+    # print(df_output)
+    for line in df_output.split('\n'):
+        if line.startswith("/dev/"):
+            pattern = re.compile("\/dev\/disk[0-999]")
+            dev_path = re.search(pattern, line.split()[0]).group(0)
+            volume_path = line.split()[8]
+            if dev_path.startswith(tuple(diskutil_list)):
+
+                disks[dev_path] = volume_path.split("/")[2]
+    # print(disks)
 
     app = QApplication([])
     app.setQuitOnLastWindowClosed(False)
@@ -33,12 +49,16 @@ def main():
 
     # Create the menu
 
-    drive = []
+    # drive.insert(i, QAction(m.group(1)))
+    # menu.addAction(drive[i])
+
     menu = QMenu()
-    for i, m in enumerate(pattern.finditer(diskutil_output)):
-        print("%s: %s" % (m.group(1), m.group(2)))
-        drive.insert(i, QAction(m.group(1)))
-        menu.addAction(drive[i])
+    items = {}
+    for disk in disks:
+        items[disk] = (QAction(disks[disk]))
+        items[disk].triggered.connect(lambda: eject_drive(disk))
+        menu.addAction(items[disk])
+        #TODO:Fix the triggered.connect call, it calls the last value of disk, not the one it was called from
 
     # Add the menu to the tray
     tray.setContextMenu(menu)
@@ -47,7 +67,8 @@ def main():
 
 
 def eject_drive(dev_path):
-    print(dev_path + "ejected.")
+    call(["diskutil", "eject", dev_path])
+    print(dev_path + " ejected.")
 
 
 if __name__ == "__main__":
